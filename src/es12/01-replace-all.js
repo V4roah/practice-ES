afc875f723fc9f1f728fd997ef1b6d6f0cd60348
v4roah1994 <64238831+V4roah@users.noreply.github.com>
2023-10-25 18:49:43 -0500

    Practice ES 12 to ES13

---
+++ src/es12/01-replace-all.js
@@ -0,0 +1,5 @@
+const string = "Javascript es un maravilloso lenguaje de programacion"
+
+const replaceString = string.replace("Javascript", "Typescript")
+
+console.log(replaceString)
---
+++ src/es13/00-at.js
@@ -0,0 +1,4 @@
+const arr = ["one", "two", "three", "four", "five", "six"]
+
+console.log(arr[arr.length - 1])
+console.log(arr.at(-1))
